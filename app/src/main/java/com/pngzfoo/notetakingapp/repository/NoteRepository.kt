fd15ed7c2c2af179d83d0586e3e852d58c37d40c
pngzfoo <tanylap_sath@hotmail.com>
2023-08-07 16:53:20 +0700

    create repository

---
+++ app/src/main/java/com/pngzfoo/notetakingapp/repository/NoteRepository.kt
@@ -0,0 +1,19 @@
+package com.pngzfoo.notetakingapp.repository
+
+import androidx.lifecycle.LiveData
+import com.pngzfoo.notetakingapp.database.NoteDatabase
+import com.pngzfoo.notetakingapp.model.Note
+//create function from noteDAO
+//
+class NoteRepository(private val db:NoteDatabase) {
+
+    suspend fun insertNote(note: Note) = db.getNoteDao().insertNote(note)
+
+    suspend fun updateNote(note: Note) = db.getNoteDao().updateNote(note)
+
+    suspend fun deleteNote(note: Note) = db.getNoteDao().deleteNote(note)
+
+    fun getAllNotes() = db.getNoteDao().getAllNotes()
+
+    fun searchNote(query:String?) = db.getNoteDao().searchNote(query)
+}
