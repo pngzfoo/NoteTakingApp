a38a65441e9ef459e9f2110da22768f902b16e65
pngzfoo <tanylap_sath@hotmail.com>
2023-08-10 15:11:45 +0700

    update note fragment

--- app/src/main/java/com/pngzfoo/notetakingapp/fragments/UpdateNoteFragment.kt
+++ app/src/main/java/com/pngzfoo/notetakingapp/fragments/UpdateNoteFragment.kt
@@ -4,30 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.pngzfoo.notetakingapp.MainActivity
 import com.pngzfoo.notetakingapp.R
+import com.pngzfoo.notetakingapp.adapter.NoteAdapter
+import com.pngzfoo.notetakingapp.databinding.FragmentHomeBinding
+import com.pngzfoo.notetakingapp.databinding.FragmentUpdateNoteBinding
+import com.pngzfoo.notetakingapp.model.Note
+import com.pngzfoo.notetakingapp.viewmodel.NoteViewModel
 
-/**
- * A simple [Fragment] subclass.
- * Use the [updateNoteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-class updateNoteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class updateNoteFragment : Fragment(R.layout.fragment_update_note) {
 
+    private var _binding : FragmentUpdateNoteBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var notesViewModel : NoteViewModel
+    private lateinit var currentNote : Note
+
+    //since update note fragment contains argument in nav_graph
+    private val args : updateNoteFragmentArgs by navArgs()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setHasOptionsMenu(true)
+
     }
 
     override fun onCreateView(
@@ -35,26 +38,33 @@ class updateNoteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_note, container, false)
+        _binding = FragmentUpdateNoteBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment updateNoteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            updateNoteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        notesViewModel = (activity as MainActivity).noteViewModel
+        currentNote = args.note!!
+
+        binding.etNoteTitleUpdate.setText(currentNote.noteTitle)
+        binding.etNoteBodyUpdate.setText(currentNote.noteBody)
+
+        //if the user update the note
+        binding.fabDone.setOnClickListener{
+            val title = binding.etNoteTitleUpdate.text.toString().trim()
+            val body = binding.etNoteBodyUpdate.text.toString().trim()
+
+            if(title.isNotEmpty()){
+                val note = Note(currentNote.id,title,body)
+                notesViewModel.updateNote(note)
+                view.findNavController().navigate(R.id.action_updateNoteFragment_to_homeFragment)
+            }else{
+                Toast.makeText(context,"Please enter note title",Toast.LENGTH_LONG).show()
             }
+        }
+
     }
+
+
 }
