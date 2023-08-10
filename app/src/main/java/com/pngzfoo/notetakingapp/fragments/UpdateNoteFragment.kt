40dc7b3684a80a9d8810d5da20a3400119f53404
pngzfoo <tanylap_sath@hotmail.com>
2023-08-10 15:31:40 +0700

    alert dialog

--- app/src/main/java/com/pngzfoo/notetakingapp/fragments/UpdateNoteFragment.kt
+++ app/src/main/java/com/pngzfoo/notetakingapp/fragments/UpdateNoteFragment.kt
@@ -1,9 +1,8 @@
 package com.pngzfoo.notetakingapp.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -64,6 +63,42 @@ class updateNoteFragment : Fragment(R.layout.fragment_update_note) {
             }
         }
 
+    }
+    private fun deleteNote(){
+        AlertDialog.Builder(activity).apply {
+            setTitle("Delete Note")
+            setMessage("Do you want to delete this note?")
+            setPositiveButton("Delete"){
+                _,_ ->
+                notesViewModel.deleteNote(currentNote)
+
+                view?.findNavController()?.navigate(
+                    R.id.action_updateNoteFragment_to_homeFragment
+                )
+            }
+            setNegativeButton("Cancle",null)
+        }.create().show()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.menu_update_note,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_delete ->{
+                deleteNote()
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
