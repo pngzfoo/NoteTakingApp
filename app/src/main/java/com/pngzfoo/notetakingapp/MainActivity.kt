eb45347d4587f4cf5ea6d601a2b0220391507e04
pngzfoo <tanylap_sath@hotmail.com>
2023-08-10 15:40:19 +0700

    main activity

--- app/src/main/java/com/pngzfoo/notetakingapp/MainActivity.kt
+++ app/src/main/java/com/pngzfoo/notetakingapp/MainActivity.kt
@@ -2,13 +2,31 @@ package com.pngzfoo.notetakingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.pngzfoo.notetakingapp.database.NoteDatabase
+import com.pngzfoo.notetakingapp.databinding.ActivityMainBinding
+import com.pngzfoo.notetakingapp.repository.NoteRepository
 import com.pngzfoo.notetakingapp.viewmodel.NoteViewModel
+import com.pngzfoo.notetakingapp.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
     lateinit var noteViewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpViewModel()
+    }
+
+    private fun setUpViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+
+        val viewModelProviderFactory = NoteViewModelFactory(application,noteRepository)
+
+        noteViewModel = ViewModelProvider(this,viewModelProviderFactory).get(NoteViewModel::class.java)
     }
 }
